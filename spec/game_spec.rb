08159021f47c8277f3ccce6b41b67533e3a5bc09
elysableu <elysabward@gmail.com>
2024-11-05 11:50:56 -0700

    Add tests for game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,6 @@
 require './spec/spec_helper'
 
-RSpec.decribe Game do
+RSpec.describe Game do
   before(:each) do
     @game = Game.new(2012030221, 20122013, 3, 6, 2, 3)
   end
@@ -18,20 +18,20 @@ RSpec.decribe Game do
       expect(@game.season).to eq(20122013)
     end
 
-    it 'has a away_team_id' do
-      expect(@game.game_id).to eq(3)
+    it 'has a away_team' do
+      expect(@game.away_team).to eq(3)
     end
 
-    it 'has a home_team_id' do
-      expect(@game.game_id).to eq(6)
+    it 'has a home_team' do
+      expect(@game.home_team).to eq(6)
     end
 
     it 'has a aways_goals' do
-      expect(@game.game_id).to eq(2)
+      expect(@game.away_goals).to eq(2)
     end
 
-    it 'has a away_team_id' do
-      expect(@game.game_id).to eq(3)
+    it 'has a home_goals' do
+      expect(@game.home_goals).to eq(3)
     end
   end
 end

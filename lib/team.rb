44d807124d577a7b0ed9f26daf27867a4eb0d1e0
elysableu <elysabward@gmail.com>
2024-11-06 13:49:49 -0700

    Update syntax

--- lib/team.rb
+++ lib/team.rb
@@ -1,10 +1,8 @@
-require 'pry'
 class Team
     attr_reader :team_id,
                 :name
                 
     def initialize(team_id, name)
-        #binding.pry
         @team_id = team_id
         @name = name
     end

41a16362ab23ab333bb48cf809bb813679785c4f
Matt Haefling <m.haefling@protonmail.com>
2024-11-05 18:44:50 -0700

    added load_games_data to the stat_tracker class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -11,11 +11,12 @@ class StatTracker
 
     def self.from_csv(locations)
         stat_tracker = StatTracker.new
-        stat_tracker.load_team_data(locations[:teams])
-        stat_tracker.load_game_data(locations[:games])
+        stat_tracker.load_teams_data(locations[:teams])
+        stat_tracker.load_games_data(locations[:games])
+        binding.pry
     end
 
-    def load_team_data(location)
+    def load_teams_data(location)
         CSV.foreach(location, headers: true, header_converters: :symbol) do |row|
             new_team = Team.new(row[:team_id], row[:teamname])
             @teams[new_team.team_id] = new_team.name
@@ -24,9 +25,8 @@ class StatTracker
     
     def load_games_data(location)
         CSV.foreach(location, headers: true, header_converters: :symbol) do |row|
-            binding.pry
-            new_game = Game.new(row[:game_id], row[:season], row[:])
-            @teams[new_team.team_id] = new_team.name
+            new_game = Game.new(row[:game_id], row[:season], row[:away_team_id], row[:home_team_id], row[:away_goals].to_i, row[:home_goals].to_i)
+            @games[new_game.game_id] = [new_game.season, new_game.away_team, new_game.home_team, new_game.away_goals, new_game.home_goals]
         end
     end
 end

f7f7cf0bd00c582249e5ffb8c3b963eea3557bc3
Matt Haefling <m.haefling@protonmail.com>
2024-11-06 17:40:37 -0700

    created total_ties, and percentage_ties method, along with their respective tests/ all tests passing

--- lib/game_statable.rb
+++ lib/game_statable.rb
@@ -28,6 +28,18 @@ module GameStatable
             game[1].hoa == "away" && game[1].result == "WIN"
         end.to_f
     end
+
+    # Counts all the key values in the game_teams hash confirming that the result value is a tie.
+    # Divides the total by two because each game is entered twice with the team id 
+    # concatenated to the end of the game_id
+    # => Float: 1517.0
+    def total_ties
+        ties = @game_teams.count do |game|
+            game[1].result == "TIE"
+        end.to_f
+        ties /= 2
+    end
+
     
     # Determines the percent of home wins by taking the result of the total_home_wins method (3237.0)
     # and dividing it by the result of the total_game_count method (7441) and then rounding the return
@@ -45,5 +57,12 @@ module GameStatable
     def percentage_visitor_wins
         visitor_wins_percent = total_visitor_wins / total_game_count
         visitor_wins_percent.round(2)
+    end
+
+    # Determines the percent of tie games by taking the result of the total_ties method (1517.0) and dividing it by the result
+    # of theh total_game_count method (7441) and then rounding the return value down to 2 decimal places.
+    def percentage_ties
+        ties_percent = total_ties / total_game_count
+        ties_percent.round(2)
     end
 end

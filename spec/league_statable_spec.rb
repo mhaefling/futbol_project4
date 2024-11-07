fe34830b32b20518c802205761bc1ecf7493a704
elysableu <elysabward@gmail.com>
2024-11-07 13:39:26 -0700

    Add tests for lowest_scoring_visitor and lowest_scoring_home_team methods

--- spec/league_statable_spec.rb
+++ spec/league_statable_spec.rb
@@ -15,10 +15,25 @@ RSpec.describe LeagueStatable do
         @stat_tracker = StatTracker.from_csv(locations)
         @stat_tracker.extend(GameStatable)
     end
+
     describe '#sort_away_teams' do
         it 'sorts games by away teams' do
             @stat_tracker.group_away_teams
-            # expect(@stat_tracker.sort_away_teams).to eq(Array)
+            expect(@stat_tracker.sort_away_teams).to eq(Array)
+        end
+    end
+
+    
+
+    describe '#lowest_scoring_visitor' do
+        it 'returns lowest scoring visitor in league' do
+            expect(@stat_tracker.lowest_scoring_visitor).to eq("San Jose Earthquakes")
+        end
+    end
+
+    describe '#lowest_scoring_home_team' do
+        it 'returns lowest scoring visitor in league' do
+            expect(@stat_tracker.lowest_scoring_home_town).to eq("Utah Royals FC")
         end
     end
 end
